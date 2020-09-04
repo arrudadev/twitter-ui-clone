d49d83a5cb37a143d20fbd012aaecbd2ec4c4c0f
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-09-03 21:49:11 -0300

    chore: add Main Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
 
+import Layout from './components/Layout';
+
 function App() {
   return (
     <>
-      <h1>Twitter UI Clone</h1>
+      <Layout />
       <GlobalStyles />
     </>
   );
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button type="button">
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Alexandre Monteiro</strong>
+          <span>612 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage /> */}
+
+      {/* <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu> */}
+    </Container>
+  );
+};
+
+export default Main;

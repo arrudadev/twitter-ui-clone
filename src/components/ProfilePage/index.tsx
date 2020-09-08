fd763933551c3c38ef75b433597876383559a7b5
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-09-08 19:17:31 -0300

    chore: add Button component and add EditButton in ProfilePage

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -8,6 +8,7 @@ import {
   LocationIcon,
   CakeIcon,
   Followage,
+  EditButton,
 } from './styles';
 
 const ProfilePage: React.FC = () => {
@@ -18,7 +19,7 @@ const ProfilePage: React.FC = () => {
       </Banner>
 
       <ProfileData>
-        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        <EditButton outlined>Editar perfil</EditButton>
 
         <h1>Alexandre Monteiro</h1>
         <h2>@ale_monteiro14</h2>
